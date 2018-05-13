f59352651877202b8e193862d88cebd408886dcc
verlil <vernigoralilia@gmail.com>
2018-05-14 01:54:41 +0300

    (*ngIf=" ; else noServer") and (<ng-template #noServer>)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,18 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+    // setInterval(() => {
+    //   console.log(this.serverName);
+    // }, 1000);
    }
 
   ngOnInit() {
   }
 
   onCreateServer(){
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created, name = '+ this.serverName;
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event){
+    //console.log(event.target.value);
+    this.serverName = event.target.value;
   }
 
 }
